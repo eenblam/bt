149d606ae3745f06ee6a3c9e42e4d73549273147
eenblam <eenblam@riseup.net>
2023-06-12 14:40:50 -0700

    Parse and validate peer messages
    Also added code generation for MType.
    
    May need to update this with `go generate` if adding a new MType

--- peer.go
+++ peer.go
@@ -2,11 +2,13 @@ package bt
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 )
 
+//go:generate stringer -type=MType
 type MType byte
 
 const (
@@ -19,6 +21,8 @@ const (
 	Request
 	Piece
 	Cancel
+	Port      // Newer, for DHT trackers
+	KeepAlive // Not an actual message id, but when length=0000
 )
 
 var HandshakePrefix = []byte(`19BitTorrent protocol00000000`)
@@ -36,10 +40,6 @@ func ParseHandshake(r io.Reader, infoHash, peerId [20]byte) error {
 	//e := NewStaticExpecter(29) // max(29, 20, 20)
 	buf := make([]byte, 29) // max(29, 20, 20)
 	return ExpectMany(r, buf, HandshakePrefix, infoHash[:], peerId[:])
-}
-
-func ParseMessage(r io.Reader) (MType, []byte, error) {
-	return Choke, nil, nil
 }
 
 func Expect(from io.Reader, buf, want []byte) error {
@@ -64,4 +64,68 @@ func ExpectMany(from io.Reader, buf []byte, wants ...[]byte) error {
 		}
 	}
 	return nil
+}
+
+type Message struct {
+	Type    MType
+	Length  uint32 // int?
+	Payload []byte
+}
+
+// ParseMessage tries to read a peer message from the reader, returning an error on failure
+func ParseMessage(r io.Reader) (*Message, error) {
+	// Parse length as BigEndian uint32
+	var length uint32
+	err := binary.Read(r, binary.BigEndian, &length)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse message length: %s", err)
+	}
+	fmt.Println(length)
+	// if 0000, it's keep-alive
+	if length == 0 {
+		return &Message{Type: KeepAlive}, nil
+	}
+	buf := make([]byte, length)
+	// Parse message id/type (one byte)
+	if _, err = io.ReadFull(r, buf); err != nil {
+		return nil, fmt.Errorf("couldn't parse message id byte: %s", err)
+	}
+	mType := MType(buf[0])
+	if length == 1 {
+		return ValidateMessage(&Message{Type: mType, Length: length})
+	} else {
+		return ValidateMessage(&Message{Type: mType, Length: length, Payload: buf[1:]})
+	}
+}
+
+// ValidateMessage ensures a message's length is appropriate for its type.
+// Checks both computed Length field and actual length of parsed Payload.
+func ValidateMessage(m *Message) (*Message, error) {
+	want := m.Length
+	switch m.Type {
+	case Choke, Unchoke, Interested, NotInterested: // No payload
+		want = 1
+	case Have: // Fixed length with payload
+		want = 5
+	case Request, Cancel: // Fixed length 13 with payload: <index><begin><length>
+		want = 13
+	case Port: // Fixed length with payload
+		// (Used in newer versions for DHT tracker)
+		want = 3
+	case Bitfield: // Variable length
+		//TODO validate against length of pieces?
+	case Piece: // Variable length: <index><begin><block...> (at least 9)
+		if m.Length < 9 {
+			return nil, fmt.Errorf("expected length >=9 for Piece message, got %d", m.Length)
+		}
+	default:
+		return nil, fmt.Errorf("unknown Message ID/Type: %b", m.Type)
+	}
+	if m.Length != want {
+		return nil, fmt.Errorf("expected length %d for %s message, got %d", want, m.Type, m.Length)
+	}
+	if len(m.Payload) != int(want)-1 {
+		return nil, fmt.Errorf("expected payload of length %d for %s message, got %d", want, m.Type, len(m.Payload))
+	}
+	return m, nil
 }
--- peer_test.go
+++ peer_test.go
@@ -60,3 +60,153 @@ func TestHandshakeSymmetry(t *testing.T) {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 }
+
+func TestParseMessage(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		Name        string
+		InputReader io.Reader
+		WantType    MType
+		WantPayload []byte
+		WantError   bool
+	}{
+		// Leave 0xff after each to confirm we didn't over-read
+		{
+			Name:        "parses Choke",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1, 0, 0xff}),
+			WantType:    Choke,
+			WantError:   false,
+		},
+		{
+			Name:        "return error for bad Choke length",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 5, 0, 0xff, 0xff, 0xff, 0xff, 0xff}),
+			WantError:   true,
+		},
+		{
+			Name:        "parses Unchoke",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1, 1, 0xff}),
+			WantType:    Unchoke,
+			WantError:   false,
+		},
+		{
+			Name:        "parses Interested",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1, 2, 0xff}),
+			WantType:    Interested,
+			WantError:   false,
+		},
+		{
+			Name:        "parses NotInterested",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1, 3, 0xff}),
+			WantType:    NotInterested,
+			WantError:   false,
+		},
+		{
+			Name:        "parses Have",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 5, 4, 1, 2, 3, 4, 0xff}),
+			WantType:    Have,
+			WantPayload: []byte{1, 2, 3, 4},
+			WantError:   false,
+		},
+		{
+			Name: "parses Bitfield", // Variable length, can just treat this like Request for now
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 13, 5,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+				0xff}),
+			WantType:    Bitfield,
+			WantPayload: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			WantError:   false,
+		},
+		{
+			Name: "parses Request",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 13, 6,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+				0xff}),
+			WantType:    Request,
+			WantPayload: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			WantError:   false,
+		},
+		{
+			Name: "parses Piece", // Same as Request if length >= 9
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 13, 7,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+				0xff}),
+			WantType:    Piece,
+			WantPayload: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			WantError:   false,
+		},
+		{
+			Name: "fails to parse Piece if length < 9", // Same as Request if length >= 9
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 8, 7,
+				1, 2, 3, 4, 5, 6, 7,
+				0xff}),
+			WantType:  Piece,
+			WantError: true,
+		},
+		{
+			Name: "parses Cancel", // same as Reques for this purpose
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 13, 8,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+				0xff}),
+			WantType:    Cancel,
+			WantPayload: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			WantError:   false,
+		},
+		{
+			Name:        "parses Port",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 3, 9, 1, 2, 0xff}),
+			WantType:    Port,
+			WantPayload: []byte{1, 2},
+			WantError:   false,
+		},
+		{
+			Name:        "parses KeepAlive",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 0, 0xff}),
+			WantType:    KeepAlive,
+			WantError:   false,
+		},
+		// Errors not specific to a message type
+		{
+			Name:        "fails if length too short",
+			InputReader: bytes.NewReader([]byte{0, 0, 0}),
+			WantError:   true,
+		},
+		{
+			Name:        "fails if not KeepAlive and no id/type byte",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1}),
+			WantError:   true,
+		},
+		{
+			Name:        "fails for unknown id/type byte",
+			InputReader: bytes.NewReader([]byte{0, 0, 0, 1, 0xff}),
+			WantError:   true,
+		},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+			m, err := ParseMessage(c.InputReader)
+			if c.WantError {
+				if err == nil {
+					t.Fatal("Wanted error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("Unexpected error: %s", err)
+			}
+			// Confirm type, payload, next byte
+			if c.WantType != m.Type {
+				t.Fatalf("want type %s, got %s", c.WantType, m.Type)
+			}
+			if !bytes.Equal(c.WantPayload, m.Payload) {
+				t.Fatalf("\n\twant payload: %x\n\tgot payload:  %x", c.WantPayload, m.Payload)
+			}
+			// Confirm the next byte is as expected
+			err = Expect(c.InputReader, []byte{0}, []byte{0xff})
+			if err != nil {
+				t.Fatalf("next byte from reader doesn't match: %s", err)
+			}
+		})
+	}
+}
